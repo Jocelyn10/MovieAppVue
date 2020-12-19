0fb1dcf79f94929c2be72b23a36f99fd3136ec1a
Jocelyn10 <jocelyn@kinshasadigital.com>
2020-12-19 21:21:20 +0100

    Add style to Home

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -22,3 +22,14 @@ export default {
   },
 };
 </script>
+<style lang="scss" scoped>
+.home {
+  display: flex;
+  flex-direction: column;
+
+  #main-section {
+    display: flex;
+    margin-top: 50px;
+  }
+}
+</style>

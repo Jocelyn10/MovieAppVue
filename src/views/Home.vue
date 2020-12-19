12bf1d996b4379ecd2ffb2eb34d734fa07a03d44
Jocelyn10 <jocelyn@kinshasadigital.com>
2020-12-19 21:19:47 +0100

    fix bug on Home

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,16 +1,24 @@
 <template>
   <div class="home">
     <NavBar />
-    <div id="main-section"></div>
+    <div id="main-section">
+      <Sidebar />
+      <MovieList />
+    </div>
   </div>
 </template>
 
 <script>
 import NavBar from "../components/Navbar";
+import MovieList from "../components/MovieList";
+import Sidebar from "../components/Sidebar";
+
 export default {
   name: "Home",
   components: {
     NavBar,
+    Sidebar,
+    MovieList,
   },
 };
 </script>

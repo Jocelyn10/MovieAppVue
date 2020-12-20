2173f766c1c8730a0627b336cb9221ced1e322db
Jocelyn10 <jocelyn@kinshasadigital.com>
2020-12-20 01:35:50 +0100

    create movies modules

--- src/store/index.js
+++ src/store/index.js
@@ -1,15 +1,34 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
+import movies from "./modules/movies"
 
 Vue.use(Vuex)
 
+const SET_USER = "SET_USER";
+
 export default new Vuex.Store({
   state: {
+    user:{
+
+    }
   },
   mutations: {
+    [SET_USER](state, user){
+      state.user = user
+    }
   },
   actions: {
+    setUser([commit], user){
+      // call API
+      commit(SET_USER, user)
+    }
   },
   modules: {
+    movies
+  },
+  getters:{
+    isUserAdmin: state => {
+      return state.user.role === 'admin'
+    }
   }
 })

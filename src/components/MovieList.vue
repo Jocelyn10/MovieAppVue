331c75d3cb2a3f78f4af2c1e7d3ae7cce1732de0
Jocelyn10 <jocelyn@kinshasadigital.com>
2020-12-20 01:19:53 +0100

    Add boucle in LovieList

--- src/components/MovieList.vue
+++ src/components/MovieList.vue
@@ -1,11 +1,36 @@
 <template>
   <div id="movie-container">
     Movie List
+
+    <ul>
+      <li style="color: white" v-for="(movie, index) in movies" :key="movie.is">
+        Index : {{ index }} {{ movie.name }}
+      </li>
+    </ul>
   </div>
 </template>
 
 <script>
-export default {};
+export default {
+  data() {
+    return {
+      movies: [
+        {
+          id: 1,
+          name: "movie-1",
+        },
+        {
+          id: 2,
+          name: "movie-2",
+        },
+        {
+          id: 3,
+          name: "movie-3",
+        },
+      ],
+    };
+  },
+};
 </script>
 
 <style lang="scss" scoped>
